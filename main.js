568ad31ff990dd3a85affa90816c0588efd64913
etrzecia <eryk.trzeciakiewicz@nokia.com>
2020-09-29 22:10:09 +0200

    implemented simple frontend

---
+++ main.js
@@ -0,0 +1,39 @@
+
+let breakpoints = [];
+
+CodeMirror.defineSimpleMode("simplemode", {
+    // The start state contains the rules that are intially used
+    start: [
+      {regex: /Load|Add|Push|Pop|Store|StoreX|StoreY|LoadX|LoadY|Subt|ShiftL|ShiftR|Output|Halt|Jump|JumpI|JnS|AddI|SubtI/,
+       token: "keyword"},
+      {regex: /DEC|HEX/, token: "atom"},
+      {regex: /0x[a-f\d]+|[-+]?(?:\.\d+|\d+\.?\d*)(?:e[-+]?\d+)?/i,
+       token: "number"},
+     
+    ],
+  });
+  
+
+var editor = CodeMirror(document.getElementById('codemirror'), {
+    value: "Load X\nAdd Y\nOutput\nHalt\nX, DEC 10\nY, HEX 0xFFFF\n",
+    mode:  "simplemode",
+    lineNumbers: true,
+    gutters: ["CodeMirror-linenumbers", "breakpoints"]
+  });
+  editor.on("gutterClick", function(cm, n) {
+    var info = cm.lineInfo(n);
+    if (!info.gutterMarkers) {
+        breakpoints.push(n)
+    }
+    cm.setGutterMarker(n, "breakpoints", info.gutterMarkers ? null : makeMarker());
+  });
+  
+  function makeMarker() {
+    var marker = document.createElement("div");
+    marker.style.color = "#822";
+    marker.innerHTML = '●';
+    return marker;
+  }
+
+  let runBtn = document.getElementById('runBtn')
+  runBtn.onclick = (e) => console.log(editor.getValue())
